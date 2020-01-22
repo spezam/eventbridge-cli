78341cce0eb3d49cc7733f633113a1dae639ea75
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2020-01-22 09:14:24 +0100

    putting together the main logic

---
+++ eventbridge.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
+)
+
+type eventbridgeClient struct {
+	client *eventbridge.Client
+
+	eventBusName string
+}
+
+func newEventbridgeClient(eventBusName string) (*eventbridgeClient, error) {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return &eventbridgeClient{
+		client:       eventbridge.New(cfg),
+		eventBusName: eventBusName,
+	}, err
+}
+
+// create temp event rule
+func (e *eventbridgeClient) createRule(ctx context.Context) (string, error) {
+	res, err := e.client.PutRuleRequest(&eventbridge.PutRuleInput{
+		Name:         aws.String(namespace + uniqueID),
+		Description:  aws.String(fmt.Sprintf("[%s] temp rule", namespace)),
+		EventBusName: aws.String(e.eventBusName),
+		EventPattern: aws.String(fmt.Sprintf(`{"source": [{"anything-but": ["%s"]}]}`, namespace)),
+		State:        eventbridge.RuleStateEnabled,
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("eventbridge.CreateRule error: %s", err)
+		return "", err
+	}
+
+	return *res.RuleArn, nil
+}
+
+func (e *eventbridgeClient) deleteRule(ctx context.Context) error {
+	_, err := e.client.DeleteRuleRequest(&eventbridge.DeleteRuleInput{
+		EventBusName: aws.String(e.eventBusName),
+		Force:        aws.Bool(true),
+		Name:         aws.String(namespace + uniqueID),
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("eventbridge.DeleteRule error: %s", err)
+		return err
+	}
+
+	return err
+}
+
+func (e *eventbridgeClient) putTarget(ctx context.Context, sqsArn string) error {
+	_, err := e.client.PutTargetsRequest(&eventbridge.PutTargetsInput{
+		Rule:         aws.String(namespace + uniqueID),,
+		EventBusName: aws.String(e.eventBusName),
+		Targets: []eventbridge.Target{
+			eventbridge.Target{
+				Id:  aws.String(namespace + uniqueID),
+				Arn: aws.String(sqsArn),
+			},
+		},
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("eventbridge.PutTarget error %s", err)
+		return err
+	}
+
+	return err
+}
+
+func (e *eventbridgeClient) removeTarget(ctx context.Context) error {
+	_, err := e.client.RemoveTargetsRequest(&eventbridge.RemoveTargetsInput{
+		Ids: []string{
+			aws.String(namespace + uniqueID),
+		},
+		Rule:         aws.String(namespace + uniqueID),
+		EventBusName: aws.String(e.eventBusName),
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("eventbridge.RemoveTarget error %s", err)
+		return err
+	}
+
+	return err
+}
---
+++ sqs.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/TylerBrock/colorjson"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+type sqsClient struct {
+	client *sqs.Client
+
+	queueURL string
+	sqsArn   string
+}
+
+func newSQSClient(ctx context.Context, ruleArn string) (*sqsClient, error) {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return nil, err
+	}
+	client := sqs.New(cfg)
+
+	// sqsArn arn:aws:sqs:eu-north-1:1234567890:eventbridge-cli-1579168041
+	a := strings.Split(ruleArn, ":")
+	region, accountID := a[3], a[4]
+	queueName := namespace + uniqueID
+	sqsArn := fmt.Sprintf("arn:aws:sqs:%s:%s:%s", region, accountID, queueName)
+
+	resp, err := client.CreateQueueRequest(&sqs.CreateQueueInput{
+		QueueName: aws.String(queueName),
+		Attributes: map[string]string{
+			"Policy": fmt.Sprintf(`{
+				"Version": "2012-10-17",
+				"Id": "%d",
+				"Statement": [{
+					"Sid": "Sid1579089564623",
+					"Effect": "Allow",
+					"Principal": {
+						"Service": "events.amazonaws.com"
+					},
+					"Action": "SQS:SendMessage",
+					"Resource": "%s",
+					"Condition": {
+						"ArnEquals": {
+							"aws:SourceArn": "%s"
+						}
+					}
+				}]
+			}`, unixTimeNow, sqsArn, ruleArn),
+		},
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("sqs.CreateQueue error: %s", err)
+		return nil, err
+	}
+
+	return &sqsClient{
+		client:   client,
+		queueURL: *resp.QueueUrl,
+		sqsArn:   sqsArn,
+	}, err
+}
+
+func (s *sqsClient) deleteQueue(ctx context.Context) error {
+	_, err := s.client.DeleteQueueRequest(&sqs.DeleteQueueInput{
+		QueueUrl: aws.String(s.queueURL),
+	}).Send(ctx)
+	if err != nil {
+		log.Printf("sqs.DeleteQueue error: %s", err)
+		return err
+	}
+
+	return err
+}
+
+func (s *sqsClient) pollQueue(ctx context.Context, breaker chan struct{}, prettyJSON bool) error {
+	log.Printf("polling queue %s ...", s.queueURL)
+	log.Printf("press ctr+c to stop")
+
+	for {
+		// goroutine
+		select {
+		case <-breaker:
+			log.Printf("stopping poller...")
+			return nil
+
+		default:
+			resp, err := s.client.ReceiveMessageRequest(&sqs.ReceiveMessageInput{
+				QueueUrl:              aws.String(s.queueURL),
+				MaxNumberOfMessages:   aws.Int64(10),
+				WaitTimeSeconds:       aws.Int64(5),
+				MessageAttributeNames: []string{"All"},
+			}).Send(ctx)
+			if err != nil {
+				log.Printf("sqs.ReceiveMessage error: %s", err)
+				return err
+			}
+
+			if len(resp.Messages) == 0 {
+				continue
+			}
+
+			entries := []sqs.DeleteMessageBatchRequestEntry{}
+			for _, m := range resp.Messages {
+				entries = append(entries, sqs.DeleteMessageBatchRequestEntry{
+					Id:            m.MessageId,
+					ReceiptHandle: m.ReceiptHandle,
+				})
+
+				if prettyJSON {
+					var j map[string]interface{}
+					err := json.Unmarshal([]byte(*m.Body), &j)
+					if err != nil {
+						return err
+					}
+
+					f := colorjson.NewFormatter()
+					f.Indent = 2
+					pj, _ := f.Marshal(j)
+
+					log.Println(string(pj))
+					continue
+				}
+
+				log.Printf("%s", *m.Body)
+			}
+
+			// cleanup messages
+			_, err = s.client.DeleteMessageBatchRequest(&sqs.DeleteMessageBatchInput{
+				QueueUrl: aws.String(s.queueURL),
+				Entries:  entries,
+			}).Send(ctx)
+			if err != nil {
+				log.Printf("sqs.DeleteMessageBatch error: %s", err)
+			}
+		}
+	}
+}
