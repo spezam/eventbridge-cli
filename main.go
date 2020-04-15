01c2d5a6cf1774e1472c8b306fa38cbf219c1a0c
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2020-04-15 20:12:12 +0200

    added ci logic

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 // EventBus --> EventBrige Rule --> SQS <-- poller
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
@@ -88,6 +90,72 @@ func run(c *cli.Context) error {
 	switch c.Command.Name {
 	case "ci":
 		log.Printf("in ci mode")
+
+		// poll SQS queue undefinitely
+		signalChan := make(chan struct{})
+		go sqsClient.pollQueueCI(c.Context, breaker, c.Bool("prettyjson"), c.Int64("timeout"))
+
+		// put event
+		time.Sleep(2 * time.Second)
+		err = ebClient.putEvent(c.Context, c.String("event"))
+		if err != nil {
+			log.Printf("removing EventBus target...")
+			ebClient.removeTarget(c.Context)
+
+			log.Printf("deleting temporary SQS queue %s...", sqsClient.queueURL)
+			sqsClient.deleteQueue(c.Context)
+
+			log.Printf("deleting temporary EventBus rule %s...", ruleArn)
+			ebClient.deleteRule(c.Context)
+
+			return err
+		}
+
+		// wait for timeout or response
+		done := make(chan bool)
+		timedOut := make(chan bool)
+		cleanupDone := make(chan struct{})
+		go func() {
+			select {
+			case <-time.After(time.Duration(c.Int64("timeout")) * time.Second):
+				log.Printf("%d seconds timeout reached", c.Int64("timeout"))
+				log.Printf("removing EventBus target...")
+				ebClient.removeTarget(c.Context)
+
+				log.Printf("deleting temporary SQS queue %s...", sqsClient.queueURL)
+				sqsClient.deleteQueue(c.Context)
+
+				log.Printf("deleting temporary EventBus rule %s...", ruleArn)
+				ebClient.deleteRule(c.Context)
+
+				timedOut <- true
+
+			case <-signalChan:
+				log.Printf("message received")
+
+				log.Printf("removing EventBus target...")
+				ebClient.removeTarget(c.Context)
+
+				log.Printf("deleting temporary SQS queue %s...", sqsClient.queueURL)
+				sqsClient.deleteQueue(c.Context)
+
+				log.Printf("deleting temporary EventBus rule %s...", ruleArn)
+				ebClient.deleteRule(c.Context)
+
+				done <- false
+			}
+
+			cleanupDone <- struct{}{}
+		}()
+		<-cleanupDone
+
+		select {
+		case <-done:
+			return nil
+		case <-timedOut:
+			return fmt.Errorf("CI failed")
+		}
+
 		log.Printf("received an interrupt, cleaning up...")
 
 		log.Printf("removing EventBus target...")
