9d3619f670e7f6a76dc3d0b225696878a5f42273
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2020-05-05 13:25:35 +0200

    v1.3.2

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 func main() {
 	app := &cli.App{
 		Name:    namespace,
-		Version: "1.3.0",
+		Version: "1.3.2",
 		Usage:   "AWS EventBridge cli",
 		Authors: []*cli.Author{
 			{Name: "matteo ridolfi"},
